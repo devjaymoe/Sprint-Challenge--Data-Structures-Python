357f0862eb9b721d5d3c26f94c97c88bdb5a5b8e
Devin_Moore <d.j.moore209@gmail.com>
2020-06-12 11:25:28 -0400

    completed ring buffer question

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,30 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.storage = []
+        self.capacity = capacity
+        self.eldest = 0
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            # overwrite at the eldest index
+            # if the eldest is at the end restart the eldest
+            if self.eldest == self.capacity:
+                self.eldest = 0
+            self.storage[self.eldest] = item
+            # increment the eldest index
+            self.eldest += 1
+        # have not reached full storage
+        else:
+            self.storage.append(item)
 
     def get(self):
-        pass
+        return self.storage
+
+# buffer = RingBuffer(5)
+
+# for i in range(20):
+#     # print(i)
+#     buffer.append(i)
+#     print(buffer.get())
+
+# print(buffer.get())
